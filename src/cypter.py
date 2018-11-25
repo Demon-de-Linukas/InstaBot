b22f47116d21254824fb995ecf4b8928031576ac
Linukas <linukaszkr@gmail.com>
2018-11-25 19:38:09 +0100

    Coding format changed

--- src/cypter.py
+++ src/cypter.py
@@ -1,24 +1,26 @@
 import random
+weight=20
+move = 50
 
 def enCode(code):
     key = int(random.random()*15)
-    engbk=bytearray(code.encode('gbk'))
+    engbk=bytearray(code.encode('utf-8'))
     result = bytearray(len(engbk)*3)
     for i in range(len(engbk)):
         origi = engbk[i]
         firsrencode = origi^key
-        third = firsrencode%16
-        fourth = firsrencode//16
-        result[i*3]=third+65
-        result[i*3+1]=fourth+65
+        third = firsrencode%weight
+        fourth = firsrencode//weight
+        result[i*3]=third+move
+        result[i*3+1]=fourth+move
         result[i*3+2]=key
-    return result.decode('gbk')
+    return result.decode('utf-8')
 
 
 def deCode(code):
-    degbk = bytearray(code.encode('gbk'))
+    degbk = bytearray(code.encode('utf-8'))
     result = bytearray(int(len(degbk)/3))
     for i in range(len(result)):
-       code =  degbk[i*3]-65+(degbk[i*3+1]-65)*16
+       code =  degbk[i*3]-move+(degbk[i*3+1]-move)*weight
        result[i] = code^degbk[i*3+2]
-    return result.decode('gbk')
+    return result.decode('utf-8')
